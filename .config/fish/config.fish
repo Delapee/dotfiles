57286a2b3e94666174c7d9f17323573f078e1ac9
Delape <delape68@gmail.com>
2025-01-27 13:08:07 +0100

    [Fish] Update fish shell configuration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,12 +2,7 @@ set fish_greeting
 
 source $HOME/.config/fish/alias
 
-set PATH $HOME/.cargo/bin $PATH
-
-set PATH $HOME/.local/share/neovim/bin $PATH
-
-set PATH $HOME/.node/bin $PATH
-
-eval /home/delape/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# set PATH $HOME/.cargo/bin $PATH
+set PATH $HOME/.local/bin $PATH
 
 starship init fish | source
